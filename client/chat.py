06fa5090b30b1fd2964ed2179e6d19a3e427684c
Thales B. Rodrigues <thalesbrito05@gmail.com>
2025-01-15 13:59:11 -0300

    feat: implement new threads

--- client/chat.py
+++ client/chat.py
@@ -3,24 +3,39 @@ import socket
 
 online = False
 
-def handle_messages(server_socket: socket.socket):
+def print_messages(server_socket: socket.socket):
     global online
 
-    if not online:
-        return
+    while True:
+        if not online:
+            continue
 
-    plaintext = server_socket.recv(1024).decode().strip()
-    print(plaintext)
+        plaintext = server_socket.recv(1024).decode().strip()
+        print(plaintext)
+
+def get_input():
+    global user_input
+    while True:
+        user_input = input()
+
+user_input = ""
 
 def chat(server_socket, keys):
     global online
+    global user_input
+
     online = True
 
-    while True:
-        threading.Thread(target=handle_messages, args=[server_socket]).start()
+    output_thread = threading.Thread(target=print_messages, args=[server_socket])
+    input_thread = threading.Thread(target=get_input)
 
-        message = input("> ")
-        if not message.strip():
+    output_thread.start()
+    input_thread.start()
+
+    while True:
+        message = user_input
+        user_input = ""
+        if not message or not message.strip():
             continue
 
         server_socket.send(message.encode())
